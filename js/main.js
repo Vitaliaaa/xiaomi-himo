099dfa31671590cab020e28512811a0dfaec8754
Vitaliaaa <112492964+Vitaliaaa@users.noreply.github.com>
2022-10-09 13:28:07 +0300

    Add files via upload
    afsdf

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.bike__slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+});
